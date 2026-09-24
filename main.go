8b15793348e45f7242db9e7730410835814c3e2f
Aaron Bittel <aaron@bittels.de>
2026-09-24 18:14:13 +0200

    So... here goes

---
+++ main.go
@@ -0,0 +1,310 @@
+package main
+
+import (
+	"math/rand/v2"
+	"strconv"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	cellSize           = 80
+	cellCount          = 9
+	width              = cellSize * cellCount
+	height             = cellSize * cellCount
+	borderThickness    = 4.0
+	highlightThickness = 9
+	selectionMargin    = 10
+
+	fontSize           = cellSize * 0.7
+	pencilMarkFontSize = cellSize * 0.2
+
+	emptyCell = 0
+)
+
+var pencilMarkCornerOffsets = [cellCount]rl.Vector2{
+	{X: 0, Y: 0},
+	{X: 2, Y: 0},
+	{X: 0, Y: 2},
+	{X: 2, Y: 2},
+	{X: 1, Y: 0},
+	{X: 1, Y: 2},
+	{X: 0, Y: 1},
+	{X: 2, Y: 1},
+	{X: 1, Y: 1},
+}
+
+type sudoku [cellCount][cellCount]cell
+
+type cell struct {
+	value    int
+	selected bool
+	corners  [cellCount]bool
+}
+
+func main() {
+	sudoku := initFilledSudoku()
+
+	rl.InitWindow(width, height, "Boxa")
+	defer rl.CloseWindow()
+
+	inSelectionMode := true
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
+			if !rl.IsKeyDown(rl.KeyLeftControl) {
+				sudoku.unselectAllCells()
+			}
+			x, y := positionToCellIdx(rl.GetMousePosition())
+			if isInsideSelectionArea(rl.GetMousePosition(), x, y) {
+				inSelectionMode = !sudoku[y][x].selected
+			}
+		}
+
+		if rl.IsMouseButtonDown(rl.MouseButtonLeft) {
+			x, y := positionToCellIdx(rl.GetMousePosition())
+			if isInsideSelectionArea(rl.GetMousePosition(), x, y) {
+				if inSelectionMode {
+					sudoku[y][x].selected = true
+				} else {
+					sudoku[y][x].selected = false
+				}
+			}
+		}
+
+		if rl.IsKeyPressed(rl.KeyBackspace) {
+			for y := range cellCount {
+				for x := range cellCount {
+					if !sudoku[y][x].selected {
+						continue
+					}
+
+					if !sudoku.isEmpty(x, y) {
+						sudoku[y][x].value = emptyCell
+					} else {
+						sudoku[y][x].corners = [cellCount]bool{}
+					}
+				}
+			}
+		}
+
+		num, pressed := getNumberPressed()
+		if pressed {
+			for y := range cellCount {
+				for x := range cellCount {
+					if !sudoku[y][x].selected {
+						continue
+					}
+
+					if rl.IsKeyDown(rl.KeyLeftControl) {
+						if sudoku.isEmpty(x, y) {
+							sudoku[y][x].corners[num-1] = !sudoku[y][x].corners[num-1]
+						}
+					} else {
+						sudoku[y][x].value = num
+					}
+				}
+			}
+		}
+
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+
+		drawSudoku(sudoku)
+		drawSelectedBorders(sudoku)
+		drawGrid()
+
+		rl.EndDrawing()
+	}
+}
+
+func drawGrid() {
+	border := rl.Rectangle{Width: width, Height: height}
+	rl.DrawRectangleLinesEx(border, borderThickness, rl.Black)
+
+	for y := 1; y < cellCount; y++ {
+		start := rl.Vector2{X: 0, Y: float32(y * cellSize)}
+		end := rl.Vector2{X: width, Y: float32(y * cellSize)}
+
+		t := float32(borderThickness / 2)
+		if y%3 == 0 {
+			t = borderThickness
+		}
+		rl.DrawLineEx(start, end, t, rl.Black)
+	}
+
+	for x := 1; x < cellCount; x++ {
+		start := rl.Vector2{X: float32(x * cellSize), Y: 0}
+		end := rl.Vector2{X: float32(x * cellSize), Y: height}
+
+		t := float32(borderThickness / 2)
+		if x%3 == 0 {
+			t = borderThickness
+		}
+		rl.DrawLineEx(start, end, t, rl.Black)
+	}
+
+}
+
+func drawSudoku(s sudoku) {
+	for y := range cellCount {
+		for x := range cellCount {
+			cellX := int32(x * cellSize)
+			cellY := int32(y * cellSize)
+
+			if !s.isEmpty(x, y) {
+				text := strconv.Itoa(s[y][x].value)
+				textWidth := rl.MeasureText(text, fontSize)
+				rl.DrawText(text, cellX+(cellSize-textWidth)/2, cellY+14, fontSize, rl.Blue)
+			} else {
+				size := int32((cellSize - 2*highlightThickness) / 3)
+				i := 0
+				for j, c := range s[y][x].corners {
+					num := j + 1
+					if c {
+						text := strconv.Itoa(num)
+						markWidth := rl.MeasureText(text, size)
+
+						offset := pencilMarkCornerOffsets[i]
+						markX := cellX + highlightThickness + int32(offset.X)*size + (size-markWidth)/2
+						markY := cellY + highlightThickness + int32(offset.Y)*size + 4
+
+						rl.DrawText(text, int32(markX), int32(markY), size, rl.Blue)
+						i++
+					}
+				}
+			}
+		}
+	}
+}
+
+func drawSelectedBorders(s sudoku) {
+	for y := range cellCount {
+		for x := range cellCount {
+			if !s[y][x].selected {
+				continue
+			}
+
+			edgesCount := 0
+
+			cellX := int32(x * cellSize)
+			cellY := int32(y * cellSize)
+
+			if y == 0 || !s[y-1][x].selected {
+				rl.DrawRectangle(cellX, cellY, cellSize, highlightThickness, rl.Blue)
+				edgesCount++
+			}
+			if y == 8 || !s[y+1][x].selected {
+				rl.DrawRectangle(cellX, cellY+cellSize-highlightThickness, cellSize, highlightThickness, rl.Blue)
+				edgesCount++
+			}
+			if x == 0 || !s[y][x-1].selected {
+				rl.DrawRectangle(cellX, cellY, highlightThickness, cellSize, rl.Blue)
+				edgesCount++
+			}
+			if x == 8 || !s[y][x+1].selected {
+				rl.DrawRectangle(cellX+cellSize-highlightThickness, cellY, highlightThickness, cellSize, rl.Blue)
+				edgesCount++
+			}
+
+			if y > 0 && x < cellCount-1 && s[y-1][x].selected && s[y][x+1].selected && !s[y-1][x+1].selected {
+				center := rl.Vector2{X: float32(cellX + cellSize), Y: float32(cellY)}
+				rl.DrawCircleSector(center, highlightThickness, 180.0, 90.0, 16, rl.Blue)
+			}
+			if y < cellCount-1 && x < cellCount-1 && s[y][x+1].selected && s[y+1][x].selected && !s[y+1][x+1].selected {
+				center := rl.Vector2{X: float32(cellX + cellSize), Y: float32(cellY + cellSize)}
+				rl.DrawCircleSector(center, highlightThickness, 180.0, 270.0, 16, rl.Blue)
+			}
+			if y < cellCount-1 && x > 0 && s[y+1][x].selected && s[y][x-1].selected && !s[y+1][x-1].selected {
+				center := rl.Vector2{X: float32(cellX), Y: float32(cellY + cellSize)}
+				rl.DrawCircleSector(center, highlightThickness, 270.0, 360.0, 16, rl.Blue)
+			}
+			if y > 0 && x > 0 && s[y][x-1].selected && s[y-1][x].selected && !s[y-1][x-1].selected {
+				center := rl.Vector2{X: float32(cellX), Y: float32(cellY)}
+				rl.DrawCircleSector(center, highlightThickness, 0.0, 90.0, 16, rl.Blue)
+			}
+		}
+	}
+}
+
+func drawSelectedCell(y, x int) {
+	rec := rl.Rectangle{
+		X:      float32(x) * cellSize,
+		Y:      float32(y) * cellSize,
+		Width:  cellSize,
+		Height: cellSize,
+	}
+	rl.DrawRectangleLinesEx(rec, borderThickness, rl.Blue)
+}
+
+func initFilledSudoku() sudoku {
+	s := sudoku{}
+	for y := range cellCount {
+		for x := range cellCount {
+			if rand.IntN(100) < 20 {
+				s[y][x].value = rand.IntN(cellCount) + 1
+			}
+		}
+	}
+	return s
+}
+
+func positionToCellIdx(pos rl.Vector2) (x, y int) {
+	return min(cellCount-1, int(pos.X/cellSize)), min(cellCount-1, int(pos.Y/cellSize))
+}
+
+func (s *sudoku) unselectAllCells() {
+	for y := range cellCount {
+		for x := range cellCount {
+			s[y][x].selected = false
+		}
+	}
+}
+
+func isInsideSelectionArea(pos rl.Vector2, x, y int) bool {
+	r1 := rl.Rectangle{
+		X:      float32(x) * cellSize,
+		Y:      float32(y)*cellSize + selectionMargin,
+		Width:  cellSize,
+		Height: cellSize - 2*selectionMargin,
+	}
+	r2 := rl.Rectangle{
+		X:      float32(x)*cellSize + selectionMargin,
+		Y:      float32(y) * cellSize,
+		Width:  cellSize - 2*selectionMargin,
+		Height: cellSize,
+	}
+
+	return rl.CheckCollisionPointRec(pos, r1) || rl.CheckCollisionPointRec(pos, r2)
+}
+
+func getNumberPressed() (int, bool) {
+	switch {
+	case rl.IsKeyPressed(rl.KeyOne), rl.IsKeyPressed(rl.KeyKp1):
+		return 1, true
+	case rl.IsKeyPressed(rl.KeyTwo), rl.IsKeyPressed(rl.KeyKp2):
+		return 2, true
+	case rl.IsKeyPressed(rl.KeyThree), rl.IsKeyPressed(rl.KeyKp3):
+		return 3, true
+	case rl.IsKeyPressed(rl.KeyFour), rl.IsKeyPressed(rl.KeyKp4):
+		return 4, true
+	case rl.IsKeyPressed(rl.KeyFive), rl.IsKeyPressed(rl.KeyKp5):
+		return 5, true
+	case rl.IsKeyPressed(rl.KeySix), rl.IsKeyPressed(rl.KeyKp6):
+		return 6, true
+	case rl.IsKeyPressed(rl.KeySeven), rl.IsKeyPressed(rl.KeyKp7):
+		return 7, true
+	case rl.IsKeyPressed(rl.KeyEight), rl.IsKeyPressed(rl.KeyKp8):
+		return 8, true
+	case rl.IsKeyPressed(rl.KeyNine), rl.IsKeyPressed(rl.KeyKp9):
+		return 9, true
+	}
+
+	return 0, false
+}
+
+func (s sudoku) isEmpty(x, y int) bool {
+	return s[y][x].value == emptyCell
+}
